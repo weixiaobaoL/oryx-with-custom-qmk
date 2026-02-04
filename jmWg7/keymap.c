bf2fdd6cbe52e1ce552ec2cd398b6158a0711534
Leo Chen <leochen@aligntech.com>
2026-02-04 13:36:12 +0800

    fix code issue

--- jmWg7/keymap.c
+++ jmWg7/keymap.c
@@ -511,10 +511,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 
 // Custom QMK here
-const key_override_t delete_key_override =
-    ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
 
-const key_override_t *key_overrides[] = (const key_override_t *[]){
-	&delete_key_override,
-	NULL
+// This globally defines all key overrides to be used
+const key_override_t *key_overrides[] = {
+	&delete_key_override
 };
