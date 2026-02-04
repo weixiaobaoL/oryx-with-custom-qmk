46c65f8238a252d6f4ba2b3bd73cfba9092c3095
Leo Chen <leochen@aligntech.com>
2026-02-04 11:40:24 +0800

    fix code issue

--- jmWg7/keymap.c
+++ jmWg7/keymap.c
@@ -514,7 +514,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const key_override_t delete_key_override =
     ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
 
-const key_override_t **key_overrides = (const key_override_t *[]){
+const key_override_t *key_overrides[] = (const key_override_t *[]){
 	&delete_key_override,
 	NULL
 };
